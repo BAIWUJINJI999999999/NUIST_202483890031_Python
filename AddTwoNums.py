38809686e2a8fbbaec8545b34aa8083d06ab9506
BAIWUJINJI999999999 <202483890031@nuist.edu.cn>
2025-03-18 20:53:41 +0800

    Version 3 AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -18,3 +18,22 @@ b = input("Second number:")
 #converting input to float and adding
 res = float(a) + float(b)
 print(res)
+
+#Add Two Numbers in Python
+#Author: Zhou Yiwen
+#Using a function
+
+#function to add two numbers
+def add(a,b):
+ #converting input to float and adding
+ result = float(a) + float(b)
+ return result
+
+#taking user input
+a = input("First number:")
+b = input("Second number:")
+
+#calling function
+res = add(a,b)
+print("The answer is:")
+print(res)
