22e8d63cbfb09a19e08cf46362ef9e84575e55d5
BAIWUJINJI999999999 <202483890031@nuist.edu.cn>
2025-03-18 22:01:28 +0800

    Version 1 AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,11 +1,8 @@
+#Add Two Numbers in Python
 #Author: Zhou Yiwen
-#Student ID: 202483890031
-#Date: 18/3/25
-
-studentNames = ["Lisa", "Liam", "Leo", "Larry", "Linda"]
-for name in studentNames:
-    print(f"{name} Evans")
-new_name = input("Enter a new name:")
-studentNames.append(new_name)
-for name in studentNames:
-    print(f"{name} Evans")
+#Using the + Operator
+a = 15
+b = 12
+#Adding two numbers
+res = a + b
+print(res)
